5a8bec8ba01a64b4ef3b69b61c3eb07d07417971
Salih800 <58995521+Salih800@users.noreply.github.com>
2021-06-21 15:07:02 +0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+while true
+do 
+printf '%s	' "$(date)" >> file.txt
+if ls -f *.plot 
+then
+echo 'File is found and sending to wasabi...' >> file.txt
+aws s3 mv --profile wasabi --endpoint-url=https://s3.eu-central-1.wasabisys.com plot-k32*.plot s3://chiasalihcakmak700/plots/
+#rclone mv *.plot wasabi:chiasalihcakmak700/plots/ -P
+else 
+echo 'File is not found' >> file.txt
+sleep 600
+fi
+done
