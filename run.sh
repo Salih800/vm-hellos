8ff9a33e867cf24dd6f0de28af6061f20edc1caa
Salih800 <58995521+Salih800@users.noreply.github.com>
2021-06-23 20:34:11 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ printf '%s	' "$(date)" >> file.txt
 if ls -f *.plot 
 then
 echo 'File is found and sending to wasabi...' >> file.txt
+ls *.plot >> /root/outputs.txt
 aws s3 mv --profile wasabi --endpoint-url=https://s3.eu-central-1.wasabisys.com plot-k32*.plot s3://chiasalihcakmak700/plots/
 #rclone mv *.plot wasabi:chiasalihcakmak700/plots/ -P
 else 
